c2ff3e60be9c151f8d9021b3d340181a5be5b1dd
Jax Herrera <jax.school52@gmail.com>
2023-04-04 19:36:48 -0700

    Commit 2?!?!

---
+++ project-3-initial-master/src/com/example/iterable/Bag.java
@@ -0,0 +1,26 @@
+package com.example.iterable;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Bag<T> implements Container<T> {
+    private ArrayList<T> items = new ArrayList<T>();
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return items.size();
+    }
+
+    @Override
+    public void add(T item) {
+        items.add(item);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return items.iterator();
+    }
+}
